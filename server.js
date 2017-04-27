1a668c5d1e72ab7e03024505bf9d2e03174c4c87
Derek Miranda <derek.miranda.1992@gmail.com>
2017-04-26 23:31:38 -0700

    Can convert video data into torrent

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 const formidable = require('formidable');
+const createTorrent = require('create-torrent');
 
 const app = express();
 const port = parseInt(process.env.PORT, 10) || 8000;
@@ -13,13 +15,26 @@ app.post('/uploadfile', (req, res) => {
   // parse files in request
   const form = new formidable.IncomingForm();
 
-  form.parse(req, (err, fields, files) => {
-    if (err) throw err;
-    console.log('Getting file...');
-    
-    res.sendStatus(200);
-  });
+  // set file upload dir
+  form.uploadDir = "uploads";
+
+  // parse form data
+  form.parse(req);
+
+  // custom data handler
+  form.onPart = function dataToTorrent (part) {
+    createTorrent(part, function(err, torrent) {
+      if (err) throw err;
+      fs.writeFile(
+        'test.torrent',
+        torrent,
+        (err) => {
+          if (err) throw err;
+        });
+    })
+  };
   
+  res.sendStatus(200);
 })
 
 app.listen(port, () => {
