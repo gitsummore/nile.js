6d5ca30507414d770614b77e4b5bcf4d87e60eaf
Derek Miranda <derek.miranda.1992@gmail.com>
2017-04-28 11:51:27 -0700

    Magnet link works if seeded off a client

--- client/getUserMedia.js
+++ client/getUserMedia.js
@@ -17,7 +17,7 @@ var client = new WebTorrent();
 // var torrentId = 'magnet:?xt=urn:btih:08ada5a7a6183aae1e09d831df6748d566095a10&dn=Sintel&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2F&xs=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel.torrent';
 
 // test torrent - Kevin's info hash
-var torrentId = 'c560772340cdb3a35233909802f05d36ae1023e8';
+var torrentId = 'magnet:?xt=urn:btih:71bc108bdc739de81b38a26a9519393413db6cb0&dn=test.webm&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com';
 
 // torrent event
 client.on('torrent', (torrent) => {
