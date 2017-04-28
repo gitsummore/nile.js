4467853f65479c3d567e65398a5591f59cccd88f
Derek Miranda <derek.miranda.1992@gmail.com>
2017-04-28 11:36:22 -0700

    Added event listeners

--- client/getUserMedia.js
+++ client/getUserMedia.js
@@ -5,25 +5,43 @@ let mediaRecorder = null;
 let chunks = [];
 let video = document.getElementById("video");
 
+// will hold
+let videoFile;
+
 // allows you to see yourself while recording
 let createSrc = (window.URL) ? window.URL.createObjectURL : function (stream) { return stream };
 
 // test playing torrent
 var client = new WebTorrent();
 // Sintel
-var torrentId = 'magnet:?xt=urn:btih:08ada5a7a6183aae1e09d831df6748d566095a10&dn=Sintel&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2F&xs=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel.torrent';
+// var torrentId = 'magnet:?xt=urn:btih:08ada5a7a6183aae1e09d831df6748d566095a10&dn=Sintel&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2F&xs=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel.torrent';
+
+// test torrent - Kevin's info hash
+var torrentId = 'c560772340cdb3a35233909802f05d36ae1023e8';
+
+// torrent event
+client.on('torrent', (torrent) => {
+  console.log('Torrent ready');
+});
+
+client.on('error', (err) => {
+  if (err) console.log(err);
+});
 
 client.add(torrentId, (torrent) => {
-  console.log('wtf');
+  console.log('why');
   // find webm file
   const file = torrent.files.find((file) => {
-    return file.name.endsWith('.mp4');
+    file.appendTo('body');
+    // return file.name.endsWith('.webm');
   });
   
   // display file in video2 tag
-  file.renderTo('video#video2');
+  // file.renderTo('video#video2');
   // file.appendTo('body');
 });
+
+console.log(client.torrents);
 
 // play button gum = GetUserMedia
 document.getElementById('button-play-gum').addEventListener('click', function () {
