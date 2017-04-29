9fadba21e92d7c46ae48888a98b7e8a1fbd65797
Derek Miranda <derek.miranda.1992@gmail.com>
2017-04-28 20:00:59 -0700

    Server can check for # of socket clients

--- socketController.js
+++ socketController.js
@@ -1,10 +1,33 @@
+// limit of initial clients to have socket connections w/
+const CLIENT_LIMIT = 3;
+
 module.exports = function (server) {
   const io = require('socket.io')(server);
 
-  io.on('connection', function(socket) {
-    console.log('Socket connection');
+  io.on('connection', function (socket) {
+    console.log('New connection');
 
-    // Emit new infohash
-    socket.emit('infohash', 'HASH');
+    // check # of clients
+    function clientHandler (err, clients) {
+      if (err) throw err;
+
+      let msg;
+      if (clients.length <= CLIENT_LIMIT) {
+        msg = 'Have a socket!';
+      } else {
+        msg = 'No socket for you!';
+
+        // TODO: if socket comes after CLIENT_LIMIT exceeded,
+        // redirect to getting hash by WebRTC
+      }
+
+      socket.emit('infohash', msg);
+    }
+
+    io.sockets.clients(clientHandler);
+
+    socket.on('disconnect', function (socket) {
+      console.log('Disconnected');
+    });
   });
 }
