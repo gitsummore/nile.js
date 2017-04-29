063e93755af5c3ff41412ce22569225a368ebe99
Derek Miranda <derek.miranda.1992@gmail.com>
2017-04-29 14:34:01 -0700

    Can start WebRTC connection and added some basic connection handlers

--- client/viewer/viewMedia.js
+++ client/viewer/viewMedia.js
@@ -2,8 +2,13 @@
 
 const socket = io.connect();
 
+let peerConn;
+
 socket.on('infohash', (msg, disconnect) => {
-  addText(msg);
+  appendText(msg);
+
+  // Start a WebRTC connection
+  webRTCInit();
 
   if (disconnect) {
     console.log('Socket disconnecting');
@@ -11,7 +16,46 @@ socket.on('infohash', (msg, disconnect) => {
   }
 });
 
-function addText(msg) {
+// TODO: on disconnect, bridge server and next-linked node
+// socket.on('disconnect')
+
+function webRTCInit() {
+  // Create WebRTC connection
+  peerConn = new RTCPeerConnection({
+    iceServers: [
+      { url: 'stun:stun.l.google.com:19302' },
+      { url: 'stun:stun1.l.google.com:19302' },
+      { url: 'stun:stun2.l.google.com:19302' },
+      { url: 'stun:stun3.l.google.com:19302' },
+      { url: 'stun:stun4.l.google.com:19302' },
+    ]
+  });
+  console.log('WebRTC connection started');
+
+  // send ICE candidates to other peer
+  // fires when RTCIceCandidate has been added to target
+  peerConn.onicecandidate = function (event) {
+    if (event.candidate) {
+      send({
+        type: 'candidate',
+        candidate: event.candidate,
+      });
+    }
+  }
+
+  peerConn.onopen = function () {
+    console.log('Connected');
+  }
+
+  peerConn.onerror = err => console.log('Error: ', err);
+}
+
+// send message on RTC connection
+function send(msg) {
+  peerConn && peerConn.send(JSON.stringify(msg));
+}
+
+function appendText(msg) {
   const pNode = document.createElement('p');
   pNode.innerText = msg;
   document.body.appendChild(pNode);
