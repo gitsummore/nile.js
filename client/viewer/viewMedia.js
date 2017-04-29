334e60839ab149e8a97889e685f93ef90ba73284
Kevin Qiu <kami.kevinq@gmail.com>
2017-04-29 11:55:34 -0700

    added utils as a script

--- client/viewer/viewMedia.js
+++ client/viewer/viewMedia.js
@@ -1,18 +1,19 @@
 // io object exposed from injected socket.io.js
-
 const socket = io.connect();
 
 socket.on('infohash', (msg, disconnect) => {
   addText(msg);
+
+  // msg is the torrentID
+  // begin downloading the torrents and render them to page, alternate between two torrents
+  if (isPlay1Playing) {
+    startDownloadingSecond(msg);
+  } else {
+    startDownloadingFirst(msg);
+  }
 
   if (disconnect) {
     console.log('Socket disconnecting');
     socket.disconnect();
   }
 });
-
-function addText(msg) {
-  const pNode = document.createElement('p');
-  pNode.innerText = msg;
-  document.body.appendChild(pNode);
-}
