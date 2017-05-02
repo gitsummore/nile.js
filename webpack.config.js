dfc91c98e2e4495192084b47c1ed5ae712ad3202
Kevin Qiu <kami.kevinq@gmail.com>
2017-05-01 20:04:35 -0700

    in middle of setting up webpack

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+// webpack.config.js
+const webpack = require('webpack')
+const path = require('path')
+
+const config = {
+  context: path.resolve(__dirname, 'client'),
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.css$/, loader: 'style-loader!css-loader' },
+    ]
+    rules: [{
+      test: /\.js$/,
+      include: path.resolve(__dirname, 'client'),
+      use: [{
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            ['es2015', { modules: false }]
+          ]
+        }
+      }]
+    }, {
+      test: /\.css$/,
+      include: path.resolve(__dirname, 'client'),
+      use: [{
+
+      }]
+    }]
+  }
+}
+
+module.exports = config
