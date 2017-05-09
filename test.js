6e9ef58725ed97655d883b903e39aef8aebdd027
jpmitchellpierson <jpmitchellpierson@gmail.com>
2017-05-08 22:36:01 -0700

    commit

--- test.js
+++ test.js
@@ -1,11 +1,14 @@
 const request = require('supertest')('http://localhost:8000');
+const should = require('should');
+const io = require('socket.io-client');
 
+//nileServer.js testing
 describe('server route testing', () => {
-  // tests uploadFile route in server.js
-  describe('POST /uploadfile', () => {
+  // tests post magnet uri route in server.js
+  describe('POST /magnet', () => {
     it('should return magnet uri as a string', done => {
     request
-        .post('/uploadfile')
+        .post('/magnet')
         .send({
           magnetURI: 'magnet:?xt=urn:btih:08ada5a7a6183aae1e09d831df6748d566095a10&dn=Sintel&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2F&xs=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel.torrent'
         })
@@ -13,3 +16,33 @@ describe('server route testing', () => {
     });
   });
 });
+
+// socket controller testing
+let options ={
+  transports: ['websocket'],
+  'force new connection': true
+};
+
+let client1 = {'name':'Tom'};
+let client2 = {'name':'Sally'};
+let client3 = {'name':'Dana'};
+
+describe("Socket Controller Testing",function(){
+  let client1 = io.connect(socketURL, options);
+  it('Callee should receive offer from new client', (done) => {
+
+  });
+
+  it('Caller should receive answer from new callee', (done) => {
+
+  });
+
+  it('Should send peers in a WebRTC connection new ICE candidates', (done) => {
+
+  });
+
+  it('Should properly remove socket from this.sockets', (done) => {
+
+  });
+});
+
