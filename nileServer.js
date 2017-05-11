84456eb7ef4be1d76ca7b196327b31a8728f6932
Derek Miranda <derek.miranda.1992@gmail.com>
2017-05-10 19:55:06 -0700

    Removed unused deps

--- nileServer.js
+++ nileServer.js
@@ -1,10 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const bodyParser = require('body-parser')
-// const formidable = require('formidable');
-// const createTorrent = require('create-torrent');
-// const WebTorrent = require('webtorrent');
+const bodyParser = require('body-parser');
 const socketController = require('./socketController');
 
 // max # of sockets to keep open
