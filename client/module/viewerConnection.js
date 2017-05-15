efd99508df7f6098f6fb18555002e27977aa27ae
Derek Miranda <derek.miranda.1992@gmail.com>
2017-05-15 14:42:06 -0700

    changed url to urls in iceServers to get rid of  deprecation warning

--- client/module/viewerConnection.js
+++ client/module/viewerConnection.js
@@ -34,11 +34,11 @@ class ViewerConnection {
     this.RTCconn = new RTCPeerConnection({
       iceServers: [
         // Default STUN servers
-        { url: 'stun:stun.l.google.com:19302' },
-        { url: 'stun:stun1.l.google.com:19302' },
-        { url: 'stun:stun2.l.google.com:19302' },
-        { url: 'stun:stun3.l.google.com:19302' },
-        { url: 'stun:stun4.l.google.com:19302' },
+        { urls: 'stun:stun.l.google.com:19302' },
+        { urls: 'stun:stun1.l.google.com:19302' },
+        { urls: 'stun:stun2.l.google.com:19302' },
+        { urls: 'stun:stun3.l.google.com:19302' },
+        { urls: 'stun:stun4.l.google.com:19302' },
         // user-provided TURN servers go here
         ...addedIceServers
       ]
