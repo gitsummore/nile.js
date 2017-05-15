12149d0b28b8f85562cf4195b8b1009284d5a4c1
Kevin Qiu <kami.kevinq@gmail.com>
2017-05-15 11:56:08 -0700

    editied terminology for nileServer

--- server.js
+++ server.js
@@ -1,27 +1,23 @@
-const express = require('expressNile');
+const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const bodyParser = require('body-parser');
-// const formidable = require('formidable');
-// const createTorrent = require('create-torrent');
-// const WebTorrent = require('webtorrent');
+
 const socketController = require('./socketController');
-// const nileServer = require('./nileServer');
+
 const app = express();
 
+const port = parseInt(process.env.PORT, 10) || 8000;
+
+// get Node Server instance when calling app.listen
 const server = app.listen(port, () => {
   console.log(`Listening on port ${port}`)
 });
 
 const nileHandler = require('./nileServer')(server);
-app.use('/', nileHandler);
 
 // parse application/json 
 app.use(bodyParser.json())
-
-const port = parseInt(process.env.PORT, 10) || 8000;
-
-// get Node Server instance when calling app.listen
 
 // Serve static files
 // app.use(express.static(path.join(__dirname, 'dist')));
@@ -29,4 +25,5 @@ app.use(express.static(path.join(__dirname, 'client')));
 
 // Routes
 // const nileHandler = nileServer(server);
+app.use('/', nileHandler);
 
